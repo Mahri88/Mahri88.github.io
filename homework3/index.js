2405d1aa9782ff5428e48c623d6cc07321143790
Mahri88 <mahri8688@gmail.com>
2024-08-18 22:25:18 -0400

    added

--- homework3/index.js
+++ homework3/index.js
@@ -20,25 +20,24 @@ const  getUser = function () {
     .then((data) => {
         const {
             picture:{large},
-            name:{ title,first,last},
-            location:{city,country},
+            name:{title, first, last},
             email,
-            cell,
-           gender,
-     }= data .results[0];
+          
+        }= data.results[0];
+        
         const usertemplate =`<div class="user-card">
     <img src="${large}" alt="Profile picters" class="profile-pic">
-    <h2 class="user-name"> ${title}${first} ${last}</h2>
+    <h2 class="user-name"> ${ title}${first} ${last}</h2>
     <p class="user-bio">  </p>
        <p class="locatoin"> <i class="fa-sharp fa-solid fa-location-dot"></i> Location: ${city } ${country}</p>
     <div class=" contact-info">
         <p class="user-email"><i class="fa-sharp fa-regular fa-envelope"></i> Email: ${email}</p>
-        <p class="gender"> Gerder: ${gender}</p>
+        <p class="gender"> Gerder: ${ age}</p>
         <p> <i class="fa-sharp fa-solid fa-phone"></i>  <strong>Phone number: ${cell}</strong></p>
     </div>
 </div>
 `;
-document.body.insertAdjacentHTML("beforeend",usertemplate)
+document.body.insertAdjacentHTML("beforeend",usertemplate);
     })
 .catch((error) => console.log(error));
     
