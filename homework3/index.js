93bc224065af1715d2f39fab5dd86da55c3968cb
Mahri88 <mahri8688@gmail.com>
2024-08-18 22:41:59 -0400

    added

--- homework3/index.js
+++ homework3/index.js
@@ -15,24 +15,26 @@
 
 
 const  getUser = function () {
-    fetch("https://randomuser.me/api/?results=10")
+    fetch("https://randomuser.me/api")
     .then((result) => result.json())
     .then((data) => {
         const {
             picture:{large},
             name:{title, first, last},
             email,
+            cell,
+            city,
+            country,
           
         }= data.results[0];
         
         const usertemplate =`<div class="user-card">
     <img src="${large}" alt="Profile picters" class="profile-pic">
-    <h2 class="user-name"> ${ title}${first} ${last}</h2>
+    <h2 class="user-name"> ${ title} ${first} ${last}</h2>
     <p class="user-bio">  </p>
        <p class="locatoin"> <i class="fa-sharp fa-solid fa-location-dot"></i> Location: ${city } ${country}</p>
     <div class=" contact-info">
         <p class="user-email"><i class="fa-sharp fa-regular fa-envelope"></i> Email: ${email}</p>
-        <p class="gender"> Gerder: ${ age}</p>
         <p> <i class="fa-sharp fa-solid fa-phone"></i>  <strong>Phone number: ${cell}</strong></p>
     </div>
 </div>
